b6b6e07f77fe10eef86c6e1ae2247ccff54e5110
Adhitvora <voraadhit2096@gmail.com>
2026-02-04 17:25:44 +0530

    Refactor email transport configuration for improved security and compatibility

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -5,13 +5,17 @@ const sendEmail = async ({ to, subject, html }) => {
     try {
         const transporter = nodemailer.createTransport({
             host: process.env.MAIL_HOST,
-            port: 465,
-            secure: true,
+            port: 587,
+            secure: false,
             auth: {
                 user: process.env.MAIL_USER,
                 pass: process.env.MAIL_PASS,
             },
+            tls: {
+                rejectUnauthorized: false
+            }
         });
+
 
         await transporter.sendMail({
             from: `"Product App" <${process.env.MAIL_USER}>`,
